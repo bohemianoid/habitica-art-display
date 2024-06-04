c026a7e36661cab0ebb8f8ae5e97e4158ac29b7f
bohemianoid <git@simonhaefliger.ch>
2024-06-05 00:20:48 +0200

    Load artwork fixed

--- resources/views/livewire/display/artwork.blade.php
+++ resources/views/livewire/display/artwork.blade.php
@@ -24,13 +24,15 @@ $refreshArtwork = function () {
 };
 ?>
 
-<div class="flex h-dvh w-full items-center justify-center">
+<div
+    class="flex h-dvh w-full items-center justify-center"
+    wire:init="loadArtwork"
+    wire:poll="refreshArtwork"
+>
     @if ($this->url)
         <img
             class="size-full object-cover"
             src="{{ $this->url }}"
-            wire:init="loadArtwork"
-            wire:poll="refreshArtwork"
         />
     @else
         <div class="size-7 animate-spin rounded-full border-4 border-b-gray-700 border-l-gray-700 border-r-gray-700 border-t-gray-200"></div>
