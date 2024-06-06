23bd18fa5a4675c8e8a98ed2eca828c8a53acadb
bohemianoid <git@simonhaefliger.ch>
2024-06-06 20:08:41 +0200

    Webhook call prune added

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Schedule;
+use Spatie\WebhookClient\Models\WebhookCall;
 
+/**
+ * Run the queue worker.
+ */
 Schedule::command('queue:work --stop-when-empty')
     ->everyMinute()
     ->withoutOverlapping();
+
+/**
+ * Delete old webhook calls.
+ */
+Schedule::command('model:prune', [
+    '--model' => [WebhookCall::class],
+])->daily();
